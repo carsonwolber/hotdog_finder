88b1e16baa87a33c806814e452b38f95ba992d1b
carsonwolber <ctw54@cornell.edu>
2024-12-11 20:10:31 -0500

    created model

--- model.py
+++ model.py
@@ -27,7 +27,27 @@ class HotDogDataset(Dataset):
   def classes(self):
     return self.data.classes
   
+transform = transforms.Compose([
+  transforms.Resize((128,128)),
+  transforms.ToTensor(),
+])
+  
 
-dataset = HotDogDataset(path)
-print(len(dataset))
+dataset = HotDogDataset(path, transform)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
+
+class HotDogClassifier(nn.Module):
+  def __init__(self, num_classes=2):
+    super(HotDogClassifier, self).__init__()
+    self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+    self.features= nn.Sequential(*list(self.base_model.children())[:-1])
+    enet_out_size = 1280
+    self.classifier = nn.Linear(enet_out_size, num_classes)
+  
+  def forward(self, x):
+    x = self.features(x)
+    output = self.classifier(x)
+
+    return output
+ 
